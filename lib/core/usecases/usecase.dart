31796ca0ae51bb19fe45921ed8d0b050a4730a9f
Nicolas Paxão da Silva <89174235+nicolaspaxao@users.noreply.github.com>
2023-09-02 14:41:58 -0300

    criando os usecases de usuario

---
+++ lib/core/usecases/usecase.dart
@@ -0,0 +1,11 @@
+import 'package:dummy_project/core/utils/typedef.dart';
+
+abstract class UsecaseWithParams<Type, Params> {
+  const UsecaseWithParams();
+  ResultFuture<Type> call(Params params);
+}
+
+abstract class UsecaseWithoutParams<Type> {
+  const UsecaseWithoutParams();
+  ResultFuture<Type> call();
+}
---
+++ lib/src/auth/domain/usecases/create_user.dart
@@ -0,0 +1,37 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:equatable/equatable.dart';
+
+import 'package:dummy_project/core/usecases/usecase.dart';
+import 'package:dummy_project/core/utils/typedef.dart';
+
+import '../repositories/auth_repository.dart';
+
+class CreateUser extends UsecaseWithParams<void, CreateUserParams> {
+  final AuthRepository _repository;
+
+  CreateUser(this._repository);
+
+  @override
+  ResultFuture call(CreateUserParams params) async {
+    return _repository.createUser(
+      createdAt: params.createdAt,
+      name: params.name,
+      avatar: params.avatar,
+    );
+  }
+}
+
+class CreateUserParams extends Equatable {
+  final String createdAt;
+  final String name;
+  final String avatar;
+
+  const CreateUserParams({
+    required this.createdAt,
+    required this.name,
+    required this.avatar,
+  });
+
+  @override
+  List<Object> get props => [createdAt, name, avatar];
+}
