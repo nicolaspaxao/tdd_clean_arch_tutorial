9865ef6d7bc5dc46fc32096082df7ced2f376528
Nicolas Paxão da Silva <89174235+nicolaspaxao@users.noreply.github.com>
2023-09-08 21:24:59 -0300

    finalizado os testes de estados

--- test/src/auth/presentation/bloc/auth_bloc_test.dart
+++ test/src/auth/presentation/bloc/auth_bloc_test.dart
@@ -1,5 +1,6 @@
 import 'package:bloc_test/bloc_test.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dummy_project/core/errors/failure.dart';
 import 'package:dummy_project/src/auth/domain/usecases/create_user.dart';
 import 'package:dummy_project/src/auth/domain/usecases/get_user.dart';
 import 'package:dummy_project/src/auth/presentation/bloc/auth_bloc.dart';
@@ -16,7 +17,9 @@ void main() {
 
   late AuthBloc bloc;
 
-  const tCreateUser = CreateUserParams.empty();
+  const CreateUserParams tCreateUser = CreateUserParams.empty();
+  const ApiFailure tApiFailure =
+      ApiFailure(message: 'message', statusCode: 400);
 
   setUp(() {
     getUsers = MockGetUsers();
@@ -35,21 +38,85 @@ void main() {
     blocTest<AuthBloc, AuthState>(
       'should emit [CreatingUser, UserCreated] when sucessful',
       build: () {
-        when(() => createUser(any())).thenAnswer(
+        when(() => createUser(tCreateUser)).thenAnswer(
           (invocation) async => const Right(null),
         );
         return bloc;
       },
       act: (bloc) => bloc.add(
-        const CreateUserEvent(
-          createdAt: 'createdAt',
-          name: 'name',
-          avatar: 'avatar',
+        CreateUserEvent(
+          createdAt: tCreateUser.createdAt,
+          name: tCreateUser.name,
+          avatar: tCreateUser.avatar,
         ),
       ),
-      expect: () => [const CreatingUser(), const UserCreated()],
-      verify: (bloc) => createUser(tCreateUser),
-      errors: () => [const AuthError('Error')],
+      expect: () => [
+        const CreatingUser(),
+        const UserCreated(),
+      ],
+      verify: (bloc) {
+        verify(() => createUser(tCreateUser));
+        verifyNoMoreInteractions(createUser);
+      },
+    );
+    blocTest<AuthBloc, AuthState>(
+      'should emit [CreatingUser, AuthError] when unsucessful',
+      build: () {
+        when(() => createUser(tCreateUser))
+            .thenAnswer((_) async => const Left(tApiFailure));
+        return bloc;
+      },
+      act: (bloc) => bloc.add(
+        CreateUserEvent(
+          createdAt: tCreateUser.createdAt,
+          name: tCreateUser.name,
+          avatar: tCreateUser.avatar,
+        ),
+      ),
+      expect: () => <AuthState>[
+        const CreatingUser(),
+        AuthError(tApiFailure.errorMessage)
+      ],
+      verify: (bloc) {
+        verify(() => createUser(tCreateUser));
+        verifyNoMoreInteractions(createUser);
+      },
+    );
+  });
+
+  group('getUser', () {
+    blocTest<AuthBloc, AuthState>(
+      'should emit [GettingUsers, UsersLoaded] when sucessful',
+      build: () {
+        when(() => getUsers()).thenAnswer((_) async => const Right([]));
+        return bloc;
+      },
+      act: (bloc) => bloc.add(const GetUsersEvent()),
+      expect: () => const <AuthState>[
+        GettingUsers(),
+        UsersLoaded([]),
+      ],
+      verify: (bloc) {
+        verify(() => getUsers()).called(1);
+        verifyNoMoreInteractions(getUsers);
+      },
+    );
+
+    blocTest<AuthBloc, AuthState>(
+      'should emit [GettingUsers, AuthError] when unsucessful',
+      build: () {
+        when(() => getUsers()).thenAnswer((_) async => const Left(tApiFailure));
+        return bloc;
+      },
+      act: (bloc) => bloc.add(const GetUsersEvent()),
+      expect: () => <AuthState>[
+        const GettingUsers(),
+        AuthError(tApiFailure.errorMessage),
+      ],
+      verify: (bloc) {
+        verify(() => getUsers()).called(1);
+        verifyNoMoreInteractions(getUsers);
+      },
     );
   });
 }
